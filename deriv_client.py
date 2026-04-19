4f21d4be3c0fdc96b66020a78436c404e5dd0ae5
Foloma <jacobfoloma8@gmail.com>
2026-04-19 18:18:08 +0100

    Fix duration assignment and update withdrawal message

--- deriv_client.py
+++ deriv_client.py
@@ -170,8 +170,9 @@ class DerivWebSocketClient:
 
             # Define duração conforme o tipo de trade
             if is_digit:
-               duration = 15
-               duration_unit = 's'
+                # Dígitos: duração de 15 segundos (alinhado com exibição lenta)
+                duration = 15
+                duration_unit = 's'
                 if contract_type == 'CALL':
                     contract_type_full = 'DIGITODD'
                 else:
